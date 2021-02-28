28e15eb25167ff930db8ce2bb9dccb0f9a58172f
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2021-02-28 10:47:11 +0000

    Parameterized run() to allow it to be tested with Strings instead of args and files

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,21 @@ use std::error::Error;
 use std::io;
 use std::process;
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let query = match env::args().nth(1) {
+fn run(
+    args : Vec<String>,
+    source : &mut dyn io::Read,
+    target : &mut dyn io::Write
+) -> Result<(), Box<dyn Error>> {
+    let query = match args.first() {
         None => return Err(From::from("expected 1 argument, but got none")),
         Some(query) => query,
     };
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    let mut wtr = csv::Writer::from_writer(io::stdout());
+    let mut rdr = csv::Reader::from_reader(source);
+    let mut wtr = csv::Writer::from_writer(target);
     wtr.write_record(rdr.headers()?)?;
     for result in rdr.records() {
         let record = result?;
-        if record.iter().any(|field| field == &query) {
+        if record.iter().any(|field| field == *query) {
             wtr.write_record(&record)?;
         }
     }
@@ -24,7 +28,11 @@ fn run() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = run() {
+    let mut args : Vec<String> = env::args().collect();
+    args.drain(0..1);  // Remove program path
+    let mut source = io::stdin();
+    let mut target = io::stdout();
+    if let Err(err) = run(args, &mut source, &mut target) {
         println!("{}", err);
         process::exit(1);
     }
