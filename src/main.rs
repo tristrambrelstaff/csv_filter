b71fb4a6c97939a2ef7019f1d66e1b4ebd5f71b5
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2021-05-29 12:00:44 +0100

    Abbreviated source and target to reduce verbosity

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use std::process;
 
 fn run(
     args : Vec<String>,
-    source : &mut dyn io::Read,
-    target : &mut dyn io::Write
+    src : &mut dyn io::Read,
+    tgt : &mut dyn io::Write
 ) -> Result<(), Box<dyn Error>> {
     let query = match args.first() {
         None => return Err(From::from("Program must be called with 1 argument")),
         Some(query) => query,
     };
-    let mut rdr = csv::Reader::from_reader(source);
-    let mut wtr = csv::Writer::from_writer(target);
+    let mut rdr = csv::Reader::from_reader(src);
+    let mut wtr = csv::Writer::from_writer(tgt);
     wtr.write_record(rdr.headers()?)?;
     for result in rdr.records() {
         let record = result?;
@@ -28,9 +28,9 @@ fn run(
 fn main() {
     let mut args : Vec<String> = env::args().collect();
     args.drain(0..1);  // Remove program path
-    let mut source = io::stdin();
-    let mut target = io::stdout();
-    if let Err(err) = run(args, &mut source, &mut target) {
+    let mut src = io::stdin();
+    let mut tgt = io::stdout();
+    if let Err(err) = run(args, &mut src, &mut tgt) {
         println!("ERROR: {}", err);
         process::exit(1);
     }
@@ -45,12 +45,12 @@ mod tests {
     #[test]
     fn pred_eq() {
         let args = vec!["a".to_string()];
-        let mut source = "a,b,c\n1,2,3\n4,a,6\n7,8,9".as_bytes();
-        let mut target : Vec<u8> = Vec::new();
-        if let Err(err) = run(args, &mut source, &mut target) {
+        let mut src = "a,b,c\n1,2,3\n4,a,6\n7,8,9".as_bytes();
+        let mut tgt : Vec<u8> = Vec::new();
+        if let Err(err) = run(args, &mut src, &mut tgt) {
             println!("ERROR: {}", err);
         } else {
-            assert_eq!(target, "a,b,c\n4,a,6\n".as_bytes().to_vec());
+            assert_eq!(tgt, "a,b,c\n4,a,6\n".as_bytes().to_vec());
 	}
     }
 
